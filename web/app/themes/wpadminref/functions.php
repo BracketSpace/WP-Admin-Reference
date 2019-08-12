0f9b4b49da09de3bf94f86579107640f44497d7a
unknown <krzysztof@bracketspace.com>
2019-08-12 16:28:15 +0200

    post list + settings table + smooth scroll

--- web/app/themes/wpadminref/functions.php
+++ web/app/themes/wpadminref/functions.php
@@ -100,7 +100,7 @@ add_filter( 'wpadminref/sections', function( $sections ) {
 		'icon'  => 'editor-justify',
 		'title' => __( 'Post List', 'wpadminref' ),
 	];
-	$sections['spinners'] = [
+	$sections['spinners']  = [
 		'icon'  => 'update-alt',
 		'title' => __( 'Spinners', 'wpadminref' ),
 	];
