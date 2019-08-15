f52c05a8f2e524bfee7479638a4500c659755f6e
unknown <krzysztof@bracketspace.com>
2019-08-15 13:44:54 +0200

    metabox column and mobile view

--- web/app/themes/wpadminref/assets/scripts/main.js
+++ web/app/themes/wpadminref/assets/scripts/main.js
@@ -1,5 +1,15 @@
 sectionMenu();
 
+Prism.plugins.NormalizeWhitespace.setDefaults({
+	'remove-trailing': true,
+	'remove-indent': true,
+	'left-trim': true,
+	'right-trim': true,
+	'break-lines': 150,
+	'indent': 0,
+	'tabs-to-spaces': 2,
+});
+
 jQuery( '.button-switch-view' ).click( function (e) {
 	e.preventDefault();
 	e.stopPropagation();
@@ -7,22 +17,23 @@ jQuery( '.button-switch-view' ).click( function (e) {
 	jQuery( this ).text() === 'Show code' ? jQuery(this).text('Show markup') : jQuery(this).text('Show code');
 
 	const section = jQuery( this ).data( 'action' );
-	const code = jQuery( section ).children( '.section-content' ).html();
+	const code = jQuery( section ).find( '.metabox-holder' ).html();
 	const section_markup_code = jQuery( section ).find( '.section-markup-code code' );
 
 	jQuery( section_markup_code ).text( code );
 	Prism.highlightAll( jQuery( section_markup_code )[0] );
 	
 	jQuery( section ).toggleClass( 'code-view' );
-
 });
 
-Prism.plugins.NormalizeWhitespace.setDefaults({
-	'remove-trailing': true,
-	'remove-indent': true,
-	'left-trim': true,
-	'right-trim': true,
-	'break-lines': 200,
+jQuery( '.section-with-columns' ).each( function( index ) {
+  const column_markup = jQuery( this ).find( '.column-content' ).html();
+  const column_code = jQuery( this ).find( '.column-code code' );
+
+  console.log(column_markup);
+
+	jQuery( column_code ).text( column_markup );
+	Prism.highlightAll( jQuery( column_code )[0] );
 });
 
 jQuery(document).on('ready', function( jQuery ){
--- web/app/themes/wpadminref/template-parts/sections/metaboxes.php
+++ web/app/themes/wpadminref/template-parts/sections/metaboxes.php
@@ -7,7 +7,7 @@
 
 ?>
 
-<section class="section section-metaboxes" id="metaboxes">
+<section class="section section-metaboxes section-with-columns" id="metaboxes">
 	<header class="section-header">
 		<h1>Metaboxes</h1>
 		<div class="switch-button">
@@ -15,8 +15,8 @@
 		</div>
 	</header>
 	<div class="section-content">
-		<div id="dashboard-widgets-wrap">
-			<div id="dashboard-widgets" class="metabox-holder">
+		<div id="dashboard-widgets-wrap" class="grid-2">
+			<div id="dashboard-widgets" class="column-content metabox-holder">
 				<div id="postbox-container-1" class="postbox-container">
 					<div id="normal-sortables" class="meta-box-sortables ui-sortable">
 						<div id="metabox" class="postbox">
@@ -25,15 +25,30 @@
 								<span class="toggle-indicator" aria-hidden="true"></span>
 							</button>
 							<h2 class="hndle ui-sortable-handle">
-								<span>Metabox title</span>
+								<span>WP-Admin reference Metabox</span>
 							</h2>
 							<div class="inside">
 								<div class="main">
-									It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
-									The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, 
-									content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum 
-									as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions 
-									have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+									<p>
+										<strong>Dummy metabox</strong>
+									</p>
+									<p>
+										It is a long established fact that a reader will be 
+										distracted by the readable content of a page when looking at its layout.
+										The point of using Lorem Ipsum is that it has a more-or-less normal 
+										distribution of letters, as opposed to using 'Content here, 
+										content here', making it look like readable English. 
+									</p>
+									<p>
+										Many desktop publishing packages and web page editors now use Lorem Ipsum 
+										as their default model text, and a search for 'lorem ipsum' will 
+										uncover many web sites still in their infancy. Various versions 
+										have evolved over the years, sometimes by accident, sometimes on 
+										purpose (injected humour and the like).
+									</p>
+									<p>
+										<a class="button button-primary">Don't click me!</a>
+									</p>
 								</div>
 							</div>
 						</div>
@@ -43,12 +58,12 @@
 					<div id="column2-sortables" class="meta-box-sortables ui-sortable empty-container" data-emptystring="Drag boxes here"></div>
 				</div>
 			</div>
+			<div class="column-code">
+				<pre class="line-numbers language-markup"><code class="line-numbers language-markup"></code></pre>
+			</div>
 		</div>
 	</div>
 	<div class="section-code">
-		<pre class="section-markup-code line-numbers language-markup"><code class="editor-code line-numbers language-markup"></code></pre>
-		<div class="section-code-functions">
-			<button class="button button-primary copy-to-clipboard">Copy to clipboard</button>
-		</div>
+		<pre class="section-markup-code line-numbers language-markup"><code class="line-numbers language-markup"></code></pre>
 	</div>
 </section>
