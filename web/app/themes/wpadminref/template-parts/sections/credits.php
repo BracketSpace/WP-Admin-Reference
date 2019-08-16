ea767740d90ba14ca9181ca7fdc4210c47c5f96b
unknown <krzysztof@bracketspace.com>
2019-08-16 13:00:50 +0200

    credist page + share buttons

--- web/app/themes/wpadminref/template-parts/sections/credits.php
+++ web/app/themes/wpadminref/template-parts/sections/credits.php
@@ -21,17 +21,26 @@
 		<h3>
 			Share:
 		</h3>
-		<div class="share-button">
-			<div class="fb-share-button" data-href="https://wpadminref.bracketspace.com/" data-layout="box_count" data-size="large">
-				<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwpadminref.bracketspace.com%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">
-					Share
+		<div class="share-buttons">
+			<div class="share-button">
+				<div class="fb-share-button" data-href="https://wpadminref.bracketspace.com/" data-layout="button" data-size="large">
+					<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwpadminref.bracketspace.com%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">
+						Share
+					</a>
+				</div>
+			</div>
+			<div class="share-button">
+				<a class="twitter-share-button"
+				  href="https://twitter.com/intent/tweet?text=Look%20what%20i%20found!%20Complete%20wp-admin%20referece%3A%20https%3A%2F%2Fwpadminref.bracketspace.com%2F"
+				  data-size="large"
+				  data-text="Share on Twitter"
+				  data-url="https://wpadminref.bracketspace.com/"
+				  data-hashtags="wp-admin,wordpress"
+				  data-via="twitterdev"
+				  data-related="twitterapi,twitter">
+					Tweet
 				</a>
 			</div>
-		</div>
-		<div class="share-button">
-			<a class="twitter-share-button" href="https://wpadminref.bracketspace.com/"= data-size="large">
-				Tweet
-			</a>
 		</div>
 	</div>
 </section>
