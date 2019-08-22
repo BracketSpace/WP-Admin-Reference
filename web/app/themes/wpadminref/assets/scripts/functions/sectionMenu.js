98aff28b1dc80ac7a1e784365f704279f62e6444
unknown <krzysztof@bracketspace.com>
2019-08-22 11:41:01 +0200

    smooth scroll fix

--- web/app/themes/wpadminref/assets/scripts/functions/sectionMenu.js
+++ web/app/themes/wpadminref/assets/scripts/functions/sectionMenu.js
@@ -5,4 +5,16 @@ function sectionMenu() {
 		activeClass : 'current',
 		threshold : 15,
 	} );
+
+	jQuery("a").on( 'click', function( e ) {
+		if ( this.hash !== '' ) {
+			e.preventDefault();
+			var hash = this.hash;
+			jQuery('html, body').animate( {
+				scrollTop: jQuery( hash ).offset().top -15
+			}, 300, function(){
+				window.location.hash = hash;
+			} );
+		}
+	});
 }
