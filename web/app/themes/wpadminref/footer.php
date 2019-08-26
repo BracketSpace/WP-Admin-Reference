679bd3e9c8c1d3d066d16bc1c6b3bd0d8007771f
Jakub Mikita <jakub@bracketspace.com>
2019-08-26 13:46:52 +0200

    phpcs fixes

--- web/app/themes/wpadminref/footer.php
+++ web/app/themes/wpadminref/footer.php
@@ -19,13 +19,13 @@
 	/* ]]> */
 </script>
 <!-- Global site tag (gtag.js) - Google Analytics -->
+<?php // phpcs:ignore ?>
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-62399988-9"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-62399988-9');
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'UA-62399988-9');
 </script>
 </body>
 </html>
