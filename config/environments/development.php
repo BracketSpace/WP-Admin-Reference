3c3d513c534d9c2b7465c01d2f71cf85494d9093
Jakub Mikita <jakub@bracketspace.com>
2019-09-19 08:14:03 +0200

    don't allow to edit files

--- config/environments/development.php
+++ config/environments/development.php
@@ -15,4 +15,4 @@ Config::define( 'SCRIPT_DEBUG', true );
 ini_set( 'display_errors', 1 );
 
 // Enable plugin and theme updates and installation from the admin
-Config::define( 'DISALLOW_FILE_MODS', false );
+Config::define( 'DISALLOW_FILE_MODS', true );
